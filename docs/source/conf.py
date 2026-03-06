e2a8220636a1618c806185df4ca56be54ddcdca6
Julie Lep <julieleproux01@gmail.com>
2026-03-06 15:34:45 +0100

    cfv

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,35 +9,35 @@
 import os
 import sys
 
-project = 'Projet orchestration'
-copyright = '2026, julie'
-author = 'julie'
-release = '0'
+project = "Projet orchestration"
+copyright = "2026, julie"
+author = "julie"
+release = "0"
 
 
-sys.path.insert(0, os.path.abspath('../..')) 
-sys.path.insert(0, os.path.abspath('../../app_api')) 
-sys.path.insert(0, os.path.abspath('../../app_front')) 
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../app_api"))
+sys.path.insert(0, os.path.abspath("../../app_front"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ 
-    'sphinx_rtd_theme', 
-    'sphinx.ext.autodoc',  # Pour extraire la doc du code 
-    'sphinx.ext.napoleon', # Pour supporter les docstrings style oogle/NumPy 
-    'sphinx.ext.mathjax',  # Pour latex 
-    "sphinx.ext.viewcode", # pour afficher code source 
-    "myst_parser",         # pour le markdown 
-] 
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",  # Pour extraire la doc du code
+    "sphinx.ext.napoleon",  # Pour supporter les docstrings style oogle/NumPy
+    "sphinx.ext.mathjax",  # Pour latex
+    "sphinx.ext.viewcode",  # pour afficher code source
+    "myst_parser",  # pour le markdown
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'fr'
+language = "fr"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
