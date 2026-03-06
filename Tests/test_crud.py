a767ee09b88e46606a60c720ec86f30157b6d709
Julie Lep <julieleproux01@gmail.com>
2026-03-06 11:16:20 +0100

    ruff

--- Tests/test_crud.py
+++ Tests/test_crud.py
@@ -6,11 +6,14 @@ from sqlalchemy.orm import sessionmaker
 
 # 1. On crée une base SQLite en mémoire pour les tests
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 2. On crée les tables dans la base de test
 Base.metadata.create_all(bind=engine)
+
 
 # 3. On remplace la dépendance get_db par notre base de test
 def override_get_db():
@@ -20,9 +23,11 @@ def override_get_db():
     finally:
         db.close()
 
+
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
+
 
 def test_create_data():
     # Ce test va maintenant écrire dans SQLite au lieu de PostgreSQL !
